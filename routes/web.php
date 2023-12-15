4ca37359b5589f5ca51bf3effcde4249206d8949
chiara1911 <chiaracesari89@gmail.com>
2023-12-15 15:40:40 +0100

    add hello world

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'text1' => 'hello',
+        'text2'=> 'world!',
+    ];
+    return view('home', $data);
 });
